0f1ec44b6aee8f7bb228074946cd09c786fa60a2
liliuhai <liliuhai@peersafe.cn>
2016-06-03 16:30:48 +0800

    Fixes
    Modify API function for Android/IOS
    Change return string,use fmt.Sprintln join result

--- cmd/ipfs_lib/ipfs_mobile.go
+++ cmd/ipfs_lib/ipfs_mobile.go
@@ -23,12 +23,12 @@ func Ipfs_cmd_arm(cmd string) string {
 	res, str, _ := Ipfs_cmd(cmd)
 
 	str = strings.Trim(str, endsep)
-	return string(res) + separtor + str
+	return fmt.Sprintf("%d%s%s", res, separtor, str)
 }
 
 func Ipfs_path(path string) string {
 	homedir.Home_Unix_Dir = path
-	return string(sucRet) + separtor + ""
+	return fmt.Sprintf("%d%s%s", sucRet, separtor, "")
 }
 
 func Ipfs_init(path string) string {
@@ -36,74 +36,74 @@ func Ipfs_init(path string) string {
 	homedir.Home_Unix_Dir = path
 	res, str, _ := Ipfs_cmd(cmd)
 	str = strings.Trim(str, endsep)
-	return string(res) + separtor + str
+	return fmt.Sprintf("%d%s%s", res, separtor, str)
 }
 
 func Ipfs_daemon() string {
 	cmd := "ipfs daemon"
 	res, str, _ := Ipfs_cmd(cmd)
 	str = strings.Trim(str, endsep)
-	return string(res) + separtor + str
+	return fmt.Sprintf("%d%s%s", res, separtor, str)
 }
 
 func Ipfs_id() string {
 	cmd := "ipfs id"
 	res, str, _ := Ipfs_cmd(cmd)
 	str = strings.Trim(str, endsep)
-	return string(res) + separtor + str
+	return fmt.Sprintf("%d%s%s", res, separtor, str)
 }
 
 func Ipfs_peerid(new_id string) string {
 	if len(new_id) != hashLen && len(new_id) != 0 {
-		return string(errRet) + separtor + ""
+		return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 	}
 
 	cmd := "ipfs config Identity.PeerID"
 	_, peerId, err := Ipfs_cmd(cmd)
 	if err != nil {
-		return string(errRet) + separtor + ""
+		return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 	}
 
 	if len(new_id) == hashLen {
 		cmd += " " + new_id
 		_, _, err := Ipfs_cmd(cmd)
 		if err != nil {
-			return string(errRet) + separtor + ""
+			return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 		}
 		peerId = new_id
 	}
 	peerId = strings.Trim(peerId, endsep)
-	return string(sucRet) + separtor + peerId
+	return fmt.Sprintf("%d%s%s", sucRet, separtor, peerId)
 }
 
 func Ipfs_privkey(new_key string) string {
 	if len(new_key) != keyLen && len(new_key) != 0 {
-		return string(errRet) + separtor + ""
+		return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 	}
 
 	cmd := "ipfs config Identity.PrivKey"
 	_, key, err := Ipfs_cmd(cmd)
 	if err != nil {
-		return string(errRet) + separtor + ""
+		return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 	}
 
 	if len(new_key) == hashLen {
 		cmd += " " + new_key
 		_, _, err := Ipfs_cmd(cmd)
 		if err != nil {
-			return string(errRet) + separtor + ""
+			return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 		}
 		key = new_key
 	}
 	key = strings.Trim(key, endsep)
-	return string(sucRet) + separtor + key
+	return fmt.Sprintf("%d%s%s", sucRet, separtor, key)
 }
 
 func Ipfs_add(os_path string) string {
 	if len(os_path) != 0 {
 		os_path, err := filepath.Abs(path.Clean(os_path))
 		if err != nil {
-			return string(errRet) + separtor + ""
+			return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 		}
 
 		fi, err := os.Lstat(os_path)
@@ -113,27 +113,27 @@ func Ipfs_add(os_path string) string {
 		} else if fi.Mode().IsRegular() {
 			cmdSuff = "ipfs add "
 		} else {
-			return string(errRet) + separtor + ""
+			return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 		}
 		fmt.Println("add cmd", cmdSuff, os_path)
 		res, addHash, err := Ipfs_cmd(cmdSuff + os_path)
 		if err != nil {
-			return string(res) + separtor + ""
+			return fmt.Sprintf("%d%s%s", res, separtor, "")
 		}
 		addHash = strings.Trim(addHash, endsep)
-		return string(res) + separtor + addHash
+		return fmt.Sprintf("%d%s%s", res, separtor, addHash)
 
 	} else {
-		return string(errRet) + separtor + ""
+		return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 	}
 }
 
 func Ipfs_get(shard_hash, os_path string) string {
 	if len(shard_hash) != hashLen {
-		return string(errRet) + separtor + ""
+		return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 	}
 	if len(os_path) == 0 {
-		return string(errRet) + separtor + ""
+		return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 	}
 
 	os_path, _ = filepath.Abs(path.Clean(os_path))
@@ -142,22 +142,22 @@ func Ipfs_get(shard_hash, os_path string) string {
 	fmt.Println("get cmd:", cmd)
 	_, _, err := Ipfs_cmd(cmd)
 	if err != nil {
-		return string(errRet) + separtor + ""
+		return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 	}
-	return string(sucRet) + separtor + ""
+	return fmt.Sprintf("%d%s%s", sucRet, separtor, "")
 }
 
 func Ipfs_publish(object_hash string) string {
 	if len(object_hash) != hashLen {
-		return string(errRet) + separtor + ""
+		return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 	}
 
 	cmd := "ipfs name publish /ipfs/" + object_hash
 	fmt.Println(cmd)
 	res, hash, err := Ipfs_cmd(cmd)
 	if err != nil {
-		return string(errRet) + separtor + ""
+		return fmt.Sprintf("%d%s%s", errRet, separtor, "")
 	}
 	hash = strings.Trim(hash, endsep)
-	return string(res) + separtor + hash
+	return fmt.Sprintf("%d%s%s", res, separtor, hash)
 }
