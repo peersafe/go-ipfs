0d82b84cd0f66af105a1217c8d2aa9b04f5c74f7
sunzhiming <sunzhiming@peersafe.cn>
2016-08-12 18:17:22 +0800

    Add test case

--- cmd/ipfs_async_test/apimain.go
+++ cmd/ipfs_async_test/apimain.go
@@ -20,13 +20,21 @@ func main() {
 	var wg sync.WaitGroup
 	done :=make(chan struct{})
 
-	// init
+	// async init
 	ipfs_lib.InitApi()
+
+	// path
+	ipfs_lib.IpfsAsyncPath("ipfs_home")
+
+	// init
+	ipfs_lib.IpfsAsyncInit(call)
 
 	// daemon
 	go func() {
 		wg.Add(1)
 		defer wg.Done()
+		
+
 		ipfs_lib.IpfsAsyncDaemon(call)
 		done <- struct{}{}
 	}()
@@ -37,26 +45,95 @@ func main() {
 		defer wg.Done()
 
 		// wait for daemon start
-		time.Sleep(5*time.Second)
+		time.Sleep(10*time.Second)
 		ipfs_lib.IpfsAsyncId(5,call)
 	}()
 
-	// add
-	// go func(){
-	// 	wg.Add(1)
-	// 	defer wg.Done()
+	// peerid
+	go func () {
+		wg.Add(1)
+		defer wg.Done()
 
-	// 	// wait for daemon start
-	// 	time.Sleep(5*time.Second)
-	// 	ipfs_lib.IpfsAsyncAdd("QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn","/xyz","apimain.go",5,call)
-	// }()
+		// wait for daemon start
+		time.Sleep(10*time.Second)
+		ipfs_lib.IpfsAsyncPeerid("",5,call)
+
+		ipfs_lib.IpfsAsyncPeerid("hello-heipi",5,call)
+
+		ipfs_lib.IpfsAsyncPeerid("",5,call)
+	}()
+
+	// privkey
+	go func () {
+		wg.Add(1)
+		defer wg.Done()
+
+		// wait for daemon start
+		time.Sleep(10*time.Second)
+		ipfs_lib.IpfsAsyncPrivkey("",5,call)
+
+		ipfs_lib.IpfsAsyncPrivkey("mykey",5,call)
+
+		ipfs_lib.IpfsAsyncPrivkey("",5,call)
+	}()
+
+	// add
+	go func(){
+		wg.Add(1)
+		defer wg.Done()
+
+		// wait for daemon start
+		time.Sleep(10*time.Second)
+		ipfs_lib.IpfsAsyncAdd("QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn","/xyz","apimain.go",5,call)
+	}()
+
+	// move
+	go func(){
+		wg.Add(1)
+		defer wg.Done()
+
+		// wait for add done 
+		time.Sleep(15*time.Second)
+		ipfs_lib.IpfsAsyncAdd("QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn","/xyz","/zyx",5,call)
+	}()
+
+	// get
+	go func(){
+		wg.Add(1)
+		defer wg.Done()
+
+		// wait for daemon start 
+		time.Sleep(10*time.Second)
+		ipfs_lib.IpfsAsyncGet("QmRVZmwRKGKVZprrqxCLHAiuqEwA9casjUA57e8pKufXNi","./getBlock",5,call)
+	}()
+
+	// query
+	go func(){
+		wg.Add(1)
+		defer wg.Done()
+
+		// wait for daemon start
+		time.Sleep(10*time.Second)
+		ipfs_lib.IpfsAsyncQuery("QmRVZmwRKGKVZprrqxCLHAiuqEwA9casjUA57e8pKufXNi","/",5,call)
+	}()
+	
+
+	// delete
+	go func(){
+		wg.Add(1)
+		defer wg.Done()
+
+		// wait for move done
+		time.Sleep(20*time.Second)
+		ipfs_lib.IpfsAsyncDelete("QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn","/zyx",5,call)
+	}()
 	
 	// shutdown
 	go func() {
 		wg.Add(1)
 		defer wg.Done()
 		// wait for all goroutine done
-		time.Sleep(10 * time.Second)
+		time.Sleep(30 * time.Second)
 		ipfs_lib.IpfsAsyncShutDown(call)
 	}()
 	wg.Wait()
